2a6be8e50d15d8d91820a11527f84b0274a0c5bd
puigmals <sergipuigmalm@gmail.com>
2022-04-10 19:38:43 +0200

    database update

--- projects/views.py
+++ projects/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    msg = "Hello, you are on the projects page"
+    number = 3
+    context = {"message": msg, "number": number}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
     #return HttpResponse(f'Aquí es mostrarà un projecte: {str(pk)}')    
     return render(request, 'projects/single-project.html')
 
+ 
