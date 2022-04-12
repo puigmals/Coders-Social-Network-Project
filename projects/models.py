aefc7ba22aba6fa6bfe9b82d2e02cd4782bc6f19
puigmals <sergipuigmalm@gmail.com>
2022-04-12 19:24:40 +0200

    database update

--- projects/models.py
+++ projects/models.py
@@ -5,8 +5,33 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
+    #així fem relació de taules many to many
+    tags = models.ManyToManyField("Tag", blank=True)
+    #mirem els vots
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (("UP", "UP VOTE"), ("DOWN", "DOWN VOTE"))
+    #owner
+    #CASCADE ELIMINA TOTES REVIEWS SI EL PROJECT ES ELIMINAT
+    #ForeignKey estableix una relacio one to many
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    def __str__(self):
+        return self.name
